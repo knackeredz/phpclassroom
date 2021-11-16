f23e23830891c3132db1fc209faff0a87467b557
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-11-16 09:29:38 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -1,20 +1,15 @@
-<html>
-  <head></head>
-  <body>
-    <table border="1">
-      <?php
-        $row = 100;
-        for($i=0;$i<$row;$i++){
-          echo "<tr>";
-          echo "<td>คอลัมน์ที่ 1</td>";
-          echo "<td>คอลัมน์ที่ 2</td>";
-          echo "<td>คอลัมน์ที่ 3</td>";
-          echo "<td>คอลัมน์ที่ 4</td>";
-          echo "<td>คอลัมน์ที่ 5</td>";
-          echo "<td>คอลัมน์ที่ 6</td>";
-          echo "</tr>";
-        }
-      ?>
-    </table>
-  </body>
-</html>
+<?php
+  echo "<table border='1'>"; //tagเปิด
+
+  for($i=0;$i<10;$i++){
+    echo "<tr>";
+    echo "<td>คอลัมน์</td>";
+    echo "<td>คอลัมน์</td>";
+    echo "<td>คอลัมน์</td>";
+    echo "<td>คอลัมน์</td>";
+    echo "</tr>";  
+  }
+
+  echo "</table>"; //tagปิด
+
+?>
