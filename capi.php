aff802b3d70dca48fb9e6e73a47d9679935632ee
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-07 09:40:49 +0700

    Update capi.php

--- capi.php
+++ capi.php
@@ -1,6 +1,6 @@
 <?php
 
-$json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all&quot;);
+$json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all&quot");
 
 $data = json_decode($json);
 
