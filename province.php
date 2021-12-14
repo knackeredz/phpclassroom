27a4397155de38606cc6c6ce87bb02f332b1bee9
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-14 09:46:57 +0700

    Update province.php

--- province.php
+++ province.php
@@ -3,7 +3,6 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
 
-  var_dump($data);
 
   foreach($data as $key=>$val){
     echo $val->province." : ".$val->new_case."<br>";
