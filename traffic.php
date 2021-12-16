3d4b7041a622e5f3ad4fd9f8941e513f656b0ea6
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-16 21:32:37 +0700

    Update traffic.php

--- traffic.php
+++ traffic.php
@@ -2,7 +2,8 @@
 <body>
   
   <div align="center">
-  <img src="https://imgur.com/rE8eejw" >
+  <blockquote class="imgur-embed-pub" lang="en" data-id="a/StCeJSO" data-context="false" ><a href="//imgur.com/a/StCeJSO"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+
 
 
 </body>
