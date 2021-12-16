08de69b7b7b954dae88a7513050a294189e5e461
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-16 21:21:42 +0700

    Update traffic.php

--- traffic.php
+++ traffic.php
@@ -1,7 +1,8 @@
 <html>
 <body>
+  
+  <blockquote class="imgur-embed-pub" lang="en" data-id="rE8eejw"><a href="https://imgur.com/rE8eejw">View post on imgur.com</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
 
-<img border="0" src="https://imgur.com/rE8eejw">
 
 </body>
 </html>
