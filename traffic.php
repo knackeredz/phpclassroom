934b3382e8e6b81c608b182b37cc6fc9f8dc4c2c
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-16 23:36:24 +0700

    Update traffic.php

--- traffic.php
+++ traffic.php
@@ -7,15 +7,12 @@ body {
 }
 </style>
   
-  
 <body>
   <meta charset="UTF-8">
   
   <div align="center">
   <img src="https://s10.gifyu.com/images/Green-and-Black-Refined-Sports-Football-Event-Banner-1.gif"
   width="100%" height="100%">
-
-
 
 </body>
 </html>
