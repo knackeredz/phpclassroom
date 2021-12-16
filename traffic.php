ea0af3be83b5a6ce699f3c240613f87d67681306
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-16 22:30:18 +0700

    Update traffic.php

--- traffic.php
+++ traffic.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html>
-<body>
-  
+
 <style>
 body {
   background-color: #ecf9f2;
 }
 </style>
   
+  
+<body>
   <div align="center">
   <img src="https://s10.gifyu.com/images/Green-and-Black-Refined-Sports-Football-Event-Banner-1.gif">
 
