10d64f3a9e55d4225d880c99a3694536521c5615
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-16 22:10:46 +0700

    Update traffic.php

--- traffic.php
+++ traffic.php
@@ -1,6 +1,13 @@
 <html>
 <body>
   
+  <script language="JavaScript">
+<!-- Begin
+//จำนวน เม็ดฝน
+var no = 50;
+//ความเร็วของฝน
+var speed = 10;
+  
   <div align="center">
   <img src="https://s10.gifyu.com/images/Green-and-Black-Refined-Sports-Football-Event-Banner-1.gif">
 
