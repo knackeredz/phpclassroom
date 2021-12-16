8aa90ee7cd3f799335b666536d29c663409c8f29
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-16 21:39:51 +0700

    Update traffic.php

--- traffic.php
+++ traffic.php
@@ -2,7 +2,7 @@
 <body>
   
   <div align="center">
-  <blockquote class="imgur-embed-pub" lang="en" data-id="a/StCeJSO" data-context="false" ><a href="//imgur.com/a/StCeJSO"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+  <img src="https://s10.gifyu.com/images/Green-and-Black-Refined-Sports-Football-Event-Banner.gif">
 
 
 
