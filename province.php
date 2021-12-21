6f5484fe7a2c7638cb53adcbb335408543499c94
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-21 10:39:26 +0700

    Update province.php

--- province.php
+++ province.php
@@ -4,33 +4,33 @@
   $data = json_decode($json);
   
   echo "<center><table border='1'>";
-  echo "<tr align='center'><td><b>ลำดับ</b></td>
-        <td><b>จังหวัด</b></td>
-        <td><b>ผู้ป่วยใหม่</b></td>
-        <td><b>ผู้ป่วยรวม</b></td>
-        <td><b>ผู้ป่วยในประเทศ</b></td>
-        <td><b>ผู้ป่วยรวมในประเทศ</b></td>
-        <td bgcolor='#ff8080'><b>ผู้เสียชีวิตใหม่</b></td>
-        <td bgcolor='#df9f9f'><b>ผู้เสียชีวิตรวม</b></td>
+  echo "<tr align='center'><td bgcolor='#218B81'><b>ลำดับ</b></td>
+        <td bgcolor='#698396'><b>จังหวัด</b></td>
+        <td bgcolor='#A9C8C0'><b>ผู้ป่วยใหม่</b></td>
+        <td bgcolor='#A9C8C0'><b>ผู้ป่วยรวม</b></td>
+        <td bgcolor='#A9C8C0'><b>ผู้ป่วยในประเทศ</b></td>
+        <td bgcolor='#A9C8C0'><b>ผู้ป่วยรวมในประเทศ</b></td>
+        <td bgcolor='#A9C8C0'><b>ผู้เสียชีวิตใหม่</b></td>
+        <td bgcolor='#AE8A8C'><b>ผู้เสียชีวิตรวม</b></td>
         <td><b>วันที่อัพเดต</b></td></tr>";
   foreach($data as $key=>$val){
     echo "<tr align='center'>";
-    echo "<td>";
+    echo "<td bgcolor='#A9C8C0'>";
     echo ($key+1);
     echo "</td>";
-    echo "<td>";
+    echo "<td bgcolor='#7C98AB'>";
     echo $val->province;
     echo "</td>";
-    echo "<td>";
+    echo "<td bgcolor='#A9C8C0'>";
     echo $val->new_case;
     echo "</td>";
-    echo "<td>";
+    echo "<td bgcolor='#A9C8C0'>";
     echo $val->total_case;
     echo "</td>";
-    echo "<td>";
+    echo "<td bgcolor='#A9C8C0'>";
     echo $val->new_case_excludeabroad;
     echo "</td>";
-    echo "<td>";
+    echo "<td bgcolor='#E5DBD9'>";
     echo $val->total_case_excludeabroad;
     echo "</td>";
     echo "<td bgcolor='#ffe6e6'>";
@@ -39,7 +39,7 @@
     echo "<td bgcolor='#f9ecec'>";
     echo $val->total_death;
     echo "</td>";
-    echo "<td>";
+    echo "<td bgcolor='#A9C8C0'";
     echo $val->update_date;
     echo "</td>";
     echo "</tr>";
