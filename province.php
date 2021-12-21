81247a01fc9f0ab7d9146cdb79175069f264b1e1
knackeredz <72967202+knackeredz@users.noreply.github.com>
2021-12-21 13:23:26 +0700

    Update province.php

--- province.php
+++ province.php
@@ -3,10 +3,9 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
-
   echo "<br><h2><center><b>รายงานสถานการณ์ COVID-19 ประจำวัน แยกตามรายจังหวัด</b></center></h2><br>";
   echo "<center><table border='1'>";
-  echo "<tr align='center'>
+  echo "<tr align='center' font color='white'>
         <td bgcolor='#413C69'><b>ลำดับ</b></td>
         <td bgcolor='#82B2B7'><b>จังหวัด</b></td>
         <td bgcolor='#4682B4'><b>ผู้ป่วยใหม่</b></td>
@@ -14,7 +13,7 @@
         <td bgcolor='#FFD700'><b>ผู้ป่วยในประเทศ</b></td>
         <td bgcolor='#FF4500'><b>ผู้ป่วยรวมในประเทศ</b></td>
         <td bgcolor='#A52A2A'><b>ผู้เสียชีวิตใหม่</b></td>
-        <td bgcolor='#9C4B28'><b>ผู้เสียชีวิตรวม</b></td></tr>";
+        <td bgcolor='#9C4B28'><b>ผู้เสียชีวิตรวม</b></td></font></tr>";
  
   foreach($data as $key=>$val){
     echo "<tr align='center'>";
