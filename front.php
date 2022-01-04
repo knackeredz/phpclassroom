3f0f453c79147868c8e2da628d0616d1e7bcb5ac
knackeredz <72967202+knackeredz@users.noreply.github.com>
2022-01-04 09:20:28 +0700

    Update front.php

--- front.php
+++ front.php
@@ -28,15 +28,22 @@
   <body>
     <div class="container">
       <div class="row">
-        <div id="col1" class="col-4">
+        <div id="col1" class="col-2">
           Column
         </div>
-        <div id="col2" class="col-4">
+        <div id="col2" class="col-2">
           Column
         </div>
-        <div id="col3" class="col-4">
+        <div id="col3" class="col-2">
           Column
         </div>
+        <div id="col1" class="col-2">
+          Column
+        </div>
+        <div id="col2" class="col-2">
+          Column
+        </div>
+        <div id="col3" class="col-2">
       </div>
     </div> 
   </body>
